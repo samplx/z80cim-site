7bf99829e3d70603351f2e6ad0be066bf2def5c6
Jim Burlingame <jb@samplx.org>
2023-09-04 10:00:54 -0500

    Add vitest support.

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+
+	test: {
+		environment: 'jsdom',
+		css: true,
+		coverage: {
+			reporter: [
+				'text',
+				'json-summary'
+			]
+		}
+	}
 });
